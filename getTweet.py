35dd4e5021b1e6a0909c3c06bfe6c3b1341539d5
JoeMay57 <joe.may3r@gmail.com>
2021-10-12 22:43:46 -0500

    Twitter API functions

--- getTweet.py
+++ getTweet.py
@@ -1,11 +1,8 @@
+from pymongo.common import SERVER_SELECTION_TIMEOUT
 import tweepy as tw
-import os
-import keyFile
+from os import environ as env
 
-def tweetPull(terms: list, inclusive:bool, noRT:bool, textOnly:bool, overWrite:bool, numtweet:int):
-    if not keyFile.fileCheck():
-        return 
-
+def tweetPull(terms: list, inclusive:bool, noRT:bool, textOnly:bool, overWrite:bool, numtweet:int, mongoURL=env.get('MongoURL')):
     if inclusive:
         x = ' '
     else:
@@ -25,39 +22,14 @@ def tweetPull(terms: list, inclusive:bool, noRT:bool, textOnly:bool, overWrite:b
         search_words = search_words + ' -has:media'
         
     try:
-        print(len(os.environ.get('Consumer_Key')))
-        auth = tw.OAuthHandler(os.environ.get('Consumer_Key'), os.environ.get('Consumer_Secret'))
-        auth.set_access_token(os.environ.get('Access_Token'), os.environ.get('Access_Token_Secret'))
+        auth = tw.OAuthHandler(env.get('Consumer_Key'), env.get('Consumer_Secret'))
+        auth.set_access_token(env.get('Access_Token'), env.get('Access_Token_Secret'))
     except:
         print('KEY ERROR\n*************\nPlease run setupt \'-s\' or \'--setup\'')
-        return 
+        return 404
 
     api = tw.API(auth, wait_on_rate_limit=True)
-    tweets = tw.Cursor(api.search_tweets, q=search_words, result_type= 'mixed', lang= 'en', include_entities= True, count=100).items(numtweet)
+    return tw.Cursor(api.search_tweets, q=search_words, result_type= 'mixed', lang= 'en', include_entities= True, count=100).items(numtweet)
     
-    from pymongo import MongoClient
-    MongoURL = os.environ.get('MongoURL')
-    class Connect(object):
-        @staticmethod    
-        def get_connection():
-            return MongoClient(MongoURL)
+    
 
-    client = Connect.get_connection()
-    db = client.tweets
-    if overWrite:
-        db.TopicTrack.drop()
-    #upload
-    for tweet in tweets:
-
-        db.TopicTrack.insert_one(
-            {"ID": tweet.id,
-            "username": tweet.user.screen_name,
-            "date": tweet.created_at,
-            "text": tweet.text,
-            "url": tweet.entities['urls'],
-            "hashtags": tweet.entities['hashtags'],
-            "geo": tweet.geo,
-            "RT": tweet.retweet_count,
-            "Fav": tweet.favorite_count}
-        )
-    return
