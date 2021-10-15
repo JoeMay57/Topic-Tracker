f50f0bfe24f1674b8faf9372ce607993ef2dfc63
JoeMay57 <joe.may3r@gmail.com>
2021-10-14 19:59:28 -0500

    Twitter API connection

--- getTweet.py
+++ getTweet.py
@@ -2,7 +2,7 @@ from pymongo.common import SERVER_SELECTION_TIMEOUT
 import tweepy as tw
 from os import environ as env
 
-def tweetPull(terms: list, inclusive:bool, noRT:bool, textOnly:bool, overWrite:bool, numtweet:int, mongoURL=env.get('MongoURL')):
+def queryGen(terms: list, inclusive:bool, noRT:bool, textOnly:bool, overWrite:bool, numtweet:int=100) -> str:
     if inclusive:
         x = ' '
     else:
@@ -20,7 +20,9 @@ def tweetPull(terms: list, inclusive:bool, noRT:bool, textOnly:bool, overWrite:b
     
     if textOnly:
         search_words = search_words + ' -has:media'
-        
+    return search_words
+
+def tweetPull(search_words:str, numtweet:int=100):
     try:
         auth = tw.OAuthHandler(env.get('Consumer_Key'), env.get('Consumer_Secret'))
         auth.set_access_token(env.get('Access_Token'), env.get('Access_Token_Secret'))
@@ -30,6 +32,3 @@ def tweetPull(terms: list, inclusive:bool, noRT:bool, textOnly:bool, overWrite:b
 
     api = tw.API(auth, wait_on_rate_limit=True)
     return tw.Cursor(api.search_tweets, q=search_words, result_type= 'mixed', lang= 'en', include_entities= True, count=100).items(numtweet)
-    
-    
-
